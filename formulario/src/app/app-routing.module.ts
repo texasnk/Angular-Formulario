e223108d174c4f34e7a4040e57c2990257585539
Davi Diniz <davirodriguesnk@gmail.com>
2023-11-09 19:09:55 -0300

    refactor: all files

--- formulario/src/app/app-routing.module.ts
+++ formulario/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { SucessoCadastroComponent } from "./sucesso-cadastro/sucesso-cadastro.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CadastroComponent } from "./cadastro/cadastro.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "cadastro",
+  },
+  {
+    path: "cadastro",
+    component: CadastroComponent,
+  },
+  {
+    path: "sucesso",
+    component: SucessoCadastroComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
